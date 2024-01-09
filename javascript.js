edfd7f94bf27b6f4d9bf9459a6d28b5deb6de64b
hala_ahmad <154763038+halaahmad123@users.noreply.github.com>
2024-01-09 23:09:06 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,38 @@
+
+  function mouseOver() {
+      document.getElementById("n").style.backgroundColor = '#0084ff';
+    }
+
+  function mouseOut() {
+      document.getElementById("n").style.backgroundColor = "#333";
+    }
+    
+  function mouseOver1() {
+    document.getElementById("m").style.backgroundColor = '#0084ff';
+  }
+
+  function mouseOut1() {
+    document.getElementById("m").style.backgroundColor = "#333";
+  }
+  
+  function mouseOver2() {
+    document.getElementById("q").style.backgroundColor = '#0084ff';
+  }
+
+  function mouseOut2() {
+    document.getElementById("q").style.backgroundColor = "#333";
+  }
+  function mouseOver3() {
+    document.getElementById("w").style.backgroundColor = '#0084ff';
+  }
+
+  function mouseOut3() {
+    document.getElementById("w").style.backgroundColor = "#333";
+  }
+  function mouseOver4() {
+    document.getElementById("e").style.backgroundColor = '#0084ff';
+  }
+
+  function mouseOut4() {
+    document.getElementById("e").style.backgroundColor = "#333";
+  }
